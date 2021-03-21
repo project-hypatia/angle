ccb179d1b2efb4758c82ad78f2e3871d266440f0
Michel Petit <contact@cogito-ergo-dev.fr>
2021-03-21 19:36:09 +0100

    Some test and todo

--- tests/DegFunctionsTest.php
+++ tests/DegFunctionsTest.php
@@ -3,6 +3,26 @@
 use Hypatia\Angle\Deg;
 
 
+test('Call to_turn as constant should success', function(){
+    $this->assertTrue(is_callable(Deg\to_turn));
+});
+test('Call to_gon as constant should success', function(){
+    $this->assertTrue(is_callable(Deg\to_gon));
+});
+test('Call to_rad as constant should success', function(){
+    $this->assertTrue(is_callable(Deg\to_rad));
+});
+test('Call to_dms as constant should success', function(){
+    $this->assertTrue(is_callable(Deg\to_dms));
+});
+
+test('Call is_right as constant should success')->skip('to do');
+test('Call is_straight as constant should success')->skip('to do');
+test('Call is_perigon as constant should success')->skip('to do');
+
+test('Call are_complementary as constant should success')->skip('to do');
+test('Call are_suplementary as constant should success')->skip('to do');
+
 test('Convert deg to float gon should success', function($deg, $gon) {
     expect(Deg\to_gon($deg))->toBe($gon);
 })->with([
