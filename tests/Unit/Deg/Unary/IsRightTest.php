1e83d6e4c29fb67986251d98e455e0ac7780b98e
Michel Petit <contact@cogito-ergo-dev.fr>
2021-03-22 09:58:26 +0100

    Unit test about Deg\is_right

--- tests/Unit/Deg/Unary/IsRightTest.php
+++ tests/Unit/Deg/Unary/IsRightTest.php
@@ -5,3 +5,13 @@ use Hypatia\Angle\Deg\Unary;
 test('Call is_right as constant should success', function(){
     $this->assertTrue(is_callable(Unary\is_right));
 });
+
+test('Call with value (-)90 should return true', function(){
+    expect(Unary\is_right(90))->toBeTrue();
+    expect(Unary\is_right(-90))->toBeTrue();
+});
+
+test('Call with value (-)270 should return true', function(){
+    expect(Unary\is_right(270))->toBeTrue();
+    expect(Unary\is_right(-270))->toBeTrue();
+});
